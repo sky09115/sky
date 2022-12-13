89bb0203e21ea753bc3e87f8f957acc7f0f86d8c
teslatk <11@qq.com>
2022-12-13 10:27:57 +0800

    ## Movie II --2022年12月10日 1. movie2 的增删改查

---
+++ main/java/com/university/demo/entity/movie2/MovieUserRatings.java
@@ -0,0 +1,28 @@
+package com.university.demo.entity.movie2;
+
+import com.baomidou.mybatisplus.annotation.*;
+import com.baomidou.mybatisplus.extension.activerecord.Model;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+@TableName("movie_user_ratings")
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class MovieUserRatings extends Model<MovieUserRatings> {
+
+    private static final long serialVersionUID=1L;
+
+    private String review_id;
+
+    private Integer douban_id;
+
+    private Integer user_id;
+
+    private Double user_movie_rating;
+}
