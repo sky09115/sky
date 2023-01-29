b2e08949a937ce77bf786dd5520eec2bd775912c
teslatk <11@qq.com>
2023-01-29 16:13:35 +0800

    ## TRAFFIC  深圳公路交通大数据 1. 后台服务端开发

---
+++ main/java/com/university/demo/entity/traffic/Info.java
@@ -0,0 +1,35 @@
+package com.university.demo.entity.traffic;
+
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
+import com.baomidou.mybatisplus.extension.activerecord.Model;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@TableName("tb_game")
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class Info extends Model<Info> {
+
+    private static final long serialVersionUID=1L;
+
+    @TableId(value = "id", type = IdType.AUTO)
+    private String id;
+
+    private String lm;
+
+    private String qs;
+
+    private String lx;
+
+    private String dj;
+
+    private Integer cd;
+
+    private String rdmj;  //路段面积
+
+    private Integer rxdmj;
+}
---
+++ main/java/com/university/demo/entity/traffic/Road.java
@@ -0,0 +1,35 @@
+package com.university.demo.entity.traffic;
+
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
+import com.baomidou.mybatisplus.extension.activerecord.Model;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@TableName("tb_road")
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class Road extends Model<Road> {
+
+    private static final long serialVersionUID=1L;
+
+    @TableId(value = "id", type = IdType.AUTO)
+    private String id;
+
+    private Double golen; //通过样本总行驶长度(m)
+
+    private Double speed; // 平均行程车速（km/h）
+
+    private String time1;
+
+    private Integer period; //时间片（一个时间片为5分钟）
+
+    private Integer blockid;
+
+    private Double gotime;  //通过样本总行程时间(s)
+
+    private Double exponent; //交通指数0-2为畅通，2-4为基本畅通，4-6为缓行，6-8为较拥堵，8-10为拥堵
+}
